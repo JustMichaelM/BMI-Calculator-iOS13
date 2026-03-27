0559a3fb68edebc0769e0b535e2ba69ab6d632c9
MichaelM <marczak.michal18@gmail.com>
2026-03-27 11:16:50 +0100

    second controller

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Michał on 27/03/2026.
+//  Copyright © 2026 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        print("Lubie Placki 2!")
+    }
+}
